ed7cabf3af6180c28191382ba1ffc8435e027ad9
Kalyani Dave <kalyanidave2004@gmail.com>
2025-04-17 21:52:50 +0530

    final commmit

--- backEnd/Routers/ExamRoutes.js
+++ backEnd/Routers/ExamRoutes.js
@@ -1,17 +1,19 @@
 const { 
     createExam, 
     deleteExam, 
-    getAllExams,     // Changed from getAllExamData
+    getAllExams,
     getExamById, 
-    updateExam
+    updateExam,
+    getUserExams    // Add this import
 } = require('../Controllers/ExamController');
 const auth = require('../middlewares/Auth');
 const router = require('express').Router();
-router.post('/create-exam',auth, createExam)
+
+router.post('/create-exam', auth, createExam)
     .put('/update-exam/:id', auth, updateExam)
     .delete('/delete-exam/:id', auth, deleteExam)
     .get('/all', auth, getAllExams)
-    .get('/:id', auth, getExamById)
-    .get('/user/:userId', auth, getUserExams);
+    .get('/user/:userId', auth, getUserExams)    // Move this before /:id
+    .get('/:id', auth, getExamById);
 
 module.exports = router;
